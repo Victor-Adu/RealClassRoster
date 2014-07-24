5bc14d120b0be1aec93c4a699805b1e42d3e02f7
Victor Adu <aduvictor@gmail.com>
2014-07-23 18:07:58 -0700

    Adding gitignore. Updating files. Homework3

---
+++ ClassRosterB19/PersonCell.swift
@@ -0,0 +1,29 @@
+//
+//  PersonCell.swift
+//  ClassRosterB19
+//
+//  Created by Victor  Adu on 7/23/14.
+//  Copyright (c) 2014 Victor  Adu. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UITableViewCell {
+
+    init(style: UITableViewCellStyle, reuseIdentifier: String) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // Initialization code
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
